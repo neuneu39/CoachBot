86c23bd27702a5529489e5b0a775c51faea72887
kan.akutsu <sakamotoshizuya@gmail.com>
2018-06-27 02:32:44 +0900

    change function name

--- src/server/route/answer.route.js
+++ src/server/route/answer.route.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  // console.log("リクエストは",req.body);
   res.json({
     weather: "いい天気ですね",
     daikichi: "今日はいいことがありますよ",
