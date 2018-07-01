daef7bcadb021bcd61a8fa198cad62432d9fb77d
kan.akutsu <sakamotoshizuya@gmail.com>
2018-07-02 01:58:22 +0900

    improve css

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -8,10 +8,23 @@
       <button type="submit">OK</button>
     </form>
   </div>
-  <div class="output-message">
-    <h2>output message</h2>
-    {{ answerText }}
+  <h2>output message</h2>
+  <div v-for="text of answerText" v-bind:key="text.id" class="output-message">
+    <div v-if=text.botFlag class="bot-message">
+      <div class="faceicon">
+        <img src="../assets/logo.png">
+      </div>
+      <div class="chatting">
+        <div class="says">
+          <p>{{ text.message }}</p>
+        </div>
+      </div>
+    </div>
+    <div v-if=!text.botFlag class="user-message">
+      <p>{{ text.message }}</p>
+    </div>
   </div>
+  <p>Back to the <router-link to="/">Home</router-link></p>
 </div>
 </template>
 
@@ -23,59 +36,120 @@ export default {
   data () {
     return {
       messageText: '',
-      answerText: ''
+      answerText: [],
+      ids: 0
     }
   },
   methods: {
     sendMessage: function () {
+      this.ids++
       if (this.messageText !== '') {
+        this.answerText.push(this.setMessage(this.ids++, this.messageText, false))
         apiService.postMessage(this.messageText)
           .then(json => {
-            this.answerText = json.message
+            this.answerText.push(this.setMessage(this.ids, json.message))
           })
       } else {
-        this.answerText = normalMessage
+        this.answerText.push(this.setMessage(this.ids, normalMessage))
+      }
+    },
+    setMessage: function (idNum, text, Flag = true) {
+      return {
+        id: idNum,
+        message: text,
+        botFlag: Flag
       }
     }
-    // getMessages: function (answer) {
-    //   apiService.getReply()
-    //     .then(message => {
-    //       this.setMessage(message[answer])
-    //       console.log(message)
-    //     })
-    // },
-    // setMessage: function (message) {
-    //   this.answerText = message
-    // },
-    // setNormalMessage: function (message) {
-    //   this.answerText = `${message}なんですね`
-    // },
-    // setKeyMessage: function () {
-    //   if (this.messageText !== '') {
-    //     this.getMessages('message')
-    //   } else {
-    //     this.setNormalMessage(this.messageText)
-    //   }
-    // }
   }
 }
 </script>
 <style>
-* {
-  box-sizing: border-box;
-}
 .message {
-  margin: 5px 0;
   width: 100%;
-  box-sizing: border-box;
   padding: 15px;
-  display: block;
 }
-.input-message, .output-message{
+.input-message, {
   width: 46%;
   border: 1px solid #ddd;
   padding: 10px;
   margin: auto;
+}
+.output-message {
+  padding: 1px 10px;
+  max-width: 450px;
+  margin: auto;
+  text-align: right;
+  font-size: 14px;
+  background: #7da4cd;
+}
+.bot-message {
+  width: 100%;
+  margin: 10px 0;
+  overflow: hidden;
+}
+.bot-message .faceicon {
+  float: left;
+  margin-right: -50px;
+  width: 40px;
+}
+.bot-message .faceicon img{
+  width: 100%;
+  height: auto;
+  border-radius: 50%;
+}
+.bot-message .chatting {
+  width: 100%;
+  text-align: left;
+}
+.says {
+  display: inline-block;
+  position: relative;
+  margin: 0 0 0 50px;
+  padding: 10px;
+  max-width: 250px;
+  border-radius: 12px;
+  background: #edf1ee;
+}
+.says::after {
+  content: "";
+  display: inline-block;
+  position: absolute;
+  top: 3px;
+  left: -19px;
+  border: 8px solid transparent;
+  border-right: 18px solid #edf1ee;
+  -ms-transform: rotate(35deg);
+  -webkit-transform: rotate(35deg);
+  transform: rotate(35deg);
+}
+.says p {
+  margin: 0;
+  padding: 0;
+}
+/*以下、③右側の緑コメント*/
+.user-message {
+  margin: 10px 0;
+}
+.user-message p{
+  display: inline-block;
+  position: relative;
+  margin: 0 10px 0 0;
+  padding: 8px;
+  max-width: 250px;
+  border-radius: 12px;
+  background: #30e852;
+  font-size: 15px;
+}
+.user-message p::after {
+  content: "";
+  position: absolute;
+  top: 3px;
+  right: -19px;
+  border: 8px solid transparent;
+  border-left: 18px solid #30e852;
+  -ms-transform: rotate(-35deg);
+  -webkit-transform: rotate(-35deg);
+  transform: rotate(-35deg);
 }
 button {
   display: block;
--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,3 +1,5 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import fetch from 'jest-fetch-mock';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+global.fetch = fetch;
