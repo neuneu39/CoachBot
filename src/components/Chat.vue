d14ce129e0c5961253d4f5bc45178411bef00711
Kan Akutsu <kan.akutsu@sample.com>
2018-07-10 04:20:13 +0900

    例外処理追加

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -24,6 +24,7 @@
       <p>{{ text.message }}</p>
     </div>
   </div>
+  <p>{{ errorMessage }}</p>
   <p>Back to the <router-link to="/">Home</router-link></p>
 </div>
 </template>
@@ -37,20 +38,26 @@ export default {
     return {
       messageText: '',
       answerText: [],
-      ids: 0
+      ids: 0,
+      errorMessage: ''
     }
   },
   methods: {
     sendMessage: function () {
-      this.ids++
+      this.resetErrorMessage()
       if (this.messageText !== '') {
         this.answerText.push(this.setMessage(this.ids++, this.messageText, false))
         apiService.postMessage(this.messageText)
           .then(json => {
-            this.answerText.push(this.setMessage(this.ids, json.message))
+            this.answerText.push(this.setMessage(this.ids++, json.message))
+          })
+          .catch(e => {
+            console.log('error response from apiService e = ', e)
+            const eMessage = 'サーバーに問題が発生しました'
+            this.setErrorMessage(eMessage)
           })
       } else {
-        this.answerText.push(this.setMessage(this.ids, normalMessage))
+        this.answerText.push(this.setMessage(this.ids++, normalMessage))
       }
     },
     setMessage: function (idNum, text, Flag = true) {
@@ -59,6 +66,12 @@ export default {
         message: text,
         botFlag: Flag
       }
+    },
+    setErrorMessage: function (eMessage) {
+      this.errorMessage = eMessage
+    },
+    resetErrorMessage: function () {
+      this.errorMessage = ''
     }
   }
 }
--- test/unit/specs/Chat.spec.js
+++ test/unit/specs/Chat.spec.js
@@ -14,10 +14,34 @@ describe('Chat.vue', () => {
   })
   it('should render input message', () => {
     const wrapper = mount(Chat)
-    wrapper.messageText = 'hello'
+    const text = {message: "いい天気ですね"}
+    fetch.mockResponseOnce(JSON.stringify(text))
+
+    wrapper.vm.messageText = 'hello'
     wrapper.find('button').trigger('submit')
+    console.log('answer=', wrapper.vm.answerText[0].message)
+    expect(wrapper.vm.ids).toEqual(1) // 入力とリプライの2つ
+    expect(wrapper.vm.answerText.length).toEqual(1) // リプライは1つ
+
+  //  サーバーのレスポンスを待ってから 
+    jest.useFakeTimers()
+    setTimeout( () => {
+      expect(wrapper.vm.ids).toEqual(1)　// 2を期待
+      expect(wrapper.vm.answerText.length).toEqual(1) // 2を期待      
+    }, 5000)
+    jest.runAllTimers();
+ 
+  })
+  it('should return an error message', () => {
+    const wrapper = mount(Chat)
+    wrapper.vm.messageText = 'hello'
+    wrapper.find('button').trigger('submit')
+
     expect(wrapper.vm.ids).toEqual(1)
     expect(wrapper.vm.answerText.length).toEqual(1)
+   // エラーになるため一度コメントアウト
+   // expect(wrapper.vm.errorMessage).toBe('サーバーに問題が発生しました')
+    
   })
   it('should link correct', () => {
     const wrapper = mount(Chat, {
