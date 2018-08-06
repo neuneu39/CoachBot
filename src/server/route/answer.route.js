bd523515b9be915655176461d2e4de4c504af973
Kan Akutsu <kan.akutsu@sample.com>
2018-08-07 02:41:51 +0900

    fix conflict

--- src/server/route/answer.route.js
+++ src/server/route/answer.route.js
@@ -8,21 +8,27 @@ const RMODE = 2;
 router.post('/', (req, res) => {
   if (req.body.message !== '') {
     if(req.body.mode === RMODE) {
-      nModeFunc(req, res)
+      rModeFunc(req, res)
+        // let mode001 = rModeFunc(req, res)
+      // if (mode001 === 1) {
+      //   mode001
+      // } else {
+      //   rModeFunc(req, res)  
+      // }
     } else {
-      rModeFunc(req,res)
+      nModeFunc(req, res)
     }
   }
 })
 
-function nModeFunc(req, res) {
+function rModeFunc(req, res) {
   const method = 'POST'
   const headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
   }
   const obj = {
-    'document': {
+    'document': { 
       'type': 'PLAIN_TEXT',
       'content': `${req.body.message}`
     },
@@ -39,10 +45,11 @@ function nModeFunc(req, res) {
     .then(response => response.json())
     .then(json => {
       console.log(json)
-      res.json(json)
+      res.json(makeResponseWord(json, req, res))
+      // res.json(`${json.entities[0].name}ってどういう意味ですか？`)
     })
 }
-function rModeFunc(req, res) {
+function nModeFunc(req, res, flag) {
   const body = new FormData()
   body.append('query', req.body.message)
   body.append('apikey', ApiKeys.talkApi)  
@@ -56,8 +63,46 @@ function rModeFunc(req, res) {
     .then(response => response.json())
     .then(json => {
       console.log(json)
-      res.json(json.results[0].reply)
+      if(flag === 1) {
+        json.results[0].reply
+      } else {
+        res.json(json.results[0].reply)
+      }
     })
+}
+
+function makeResponseWord(dataJson, req, res) {
+	let entitie = dataJson.entities
+	let sen_magnitude = dataJson.documentSentiment.magnitude
+	let sen_score = dataJson.documentSentiment.score
+	console.log("sen score", sen_score, sen_magnitude)
+  let ImportWord = '...'
+  const Flag = 1
+
+	if(sen_magnitude > 0.1 && sen_score > 0.3) {
+		ImportWord = findImportantWord(entitie)
+		console.log("1=", ImportWord.name　+ 'ってどういうことですか。もっと聞かせてください');
+		return ImportWord.name + 'ってどういうことですか。もっと聞かせてください'
+	} else if (sen_magnitude > 0.1 && sen_score < -0.3) {
+		ImportWord = findImportantWord(entitie)
+		console.log("2=", 'そうなんですね' + ImportWord.name + 'ってどんなことですか');
+		return 'そうなんですね' + ImportWord.name + 'ってどんなことですか'
+	} else {
+    // if (Math.random() > 0.1) {
+    //   //let words = nModeFunc(req, res, flag = 1)
+    //   //console.log('words = ', words)
+    //   return Flag
+    // } else {
+    return ImportWord
+	}
+}
+
+function findImportantWord(entitie) {
+	const MaxSalience = Math.max(...entitie.map((o) => o.salience));
+	const importantEntitie = entitie.filter((value) => {
+	    return value.salience === MaxSalience
+	});
+	return importantEntitie[0] //最初の単語を
 }
 
 module.exports = router
